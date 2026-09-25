9ff1a6d53eb9e54f2321fe8cdafbf27894d9473c
ppgwx <tc10762@gmail.com>
2026-09-25 23:03:18 +0300

    chore: enable initial admin creation on deploy
    Add conditional superuser creation in entrypoint script

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -4,4 +4,10 @@ set -eu
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
+if [ -n "${DJANGO_SUPERUSER_USERNAME:-}" ] && \
+   [ -n "${DJANGO_SUPERUSER_EMAIL:-}" ] && \
+   [ -n "${DJANGO_SUPERUSER_PASSWORD:-}" ]; then
+    python manage.py createsuperuser --noinput || true
+fi
+
 exec "$@"
